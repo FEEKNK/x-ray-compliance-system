79874b25d2749376866edb4c9f8aca0e1757300d
FEEKNK <brkk_3804@hotmail.com>
2026-06-02 10:37:02 +0700

    fix: express 5 wildcard route crash on startup

--- server/index.ts
+++ server/index.ts
@@ -301,7 +301,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const distPath = path.join(__dirname, '../dist');
 app.use(express.static(distPath));
-app.get('*', (_req, res) => {
+app.use((_req, res) => {
   res.sendFile(path.join(distPath, 'index.html'));
 });
 
